83eb6a809fc400bd2b7998db2a1a7944e24cb39c
Mordecai K. <39312305+insidemordecai@users.noreply.github.com>
2025-11-17 09:46:54 +0300

    new(bootcamp): new files

---
+++ bootcamp-python/code1.py
@@ -0,0 +1,18 @@
+# This is my first Python code
+# variables are simple containers that store data of different types
+
+# data types
+favouriteNumber = 2 # integer (int)
+numberFloat = 2.1 # float 
+name = 'Kyle' # string (str)
+doesHePlayRoblox = True # boolean (bool) - true/false
+favouriteFruits = ["pawpaw", "apple", "grapes"] # list
+
+favouriteFruits.append("banana") # adding an item to the end of the list
+favouriteFruits[0], favouriteFruits[1] = favouriteFruits[1], favouriteFruits[0] # how to swap elements 0 and 1
+print(favouriteFruits)
+
+print("My name is", name, "and my favourite fruits are", favouriteFruits)
+
+name = "Merossa" # changing content of the variable 'name'
+print("My name is", name, "and my favourite fruits are", favouriteFruits)
