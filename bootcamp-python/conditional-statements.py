6784d665dde837369e93b870c3f78666625e7fe1
Mordecai K. <39312305+insidemordecai@users.noreply.github.com>
2025-11-18 10:28:45 +0300

    update during class

--- bootcamp-python/conditional-statements.py
+++ bootcamp-python/conditional-statements.py
@@ -1,12 +1,47 @@
 # Conditional Statements 
 # They check if a certain condition is true or false and execute different block of code accordingly
 
-age = 25
+age = 15
 
 # we want to check for teenagers (age 13 to 19)
-if age >= 13 and age <= 19: # or age > 12
+if age >= 13 and age <= 19:
   print("You are in the teen club")
 elif age > 19:
   print("You are too old 😂")
 else:
   print("Sorry, you need to be older to join the club!")
+
+# task: try the above age comparison using a different code
+# OPTION 1
+# if age >= 13
+#   if age >= 20
+#     too old
+#   else
+#     teen club
+# else
+#   too young
+
+if age >= 13:
+  if age >= 20:
+    print("You are too old 😂")
+  else:
+    print("Welcome to the teen club.")
+else:
+  print("You are too young 🙂")
+
+# OPTION 2
+# if age >= 13 and age <= 19 
+#   teen club
+# else
+#   if age >= 20 
+#     too old
+#   else 
+#     too young
+
+if age >= 13 and age <= 19:
+  print("Welcome to the teen club")
+else: 
+  if age >= 20:
+    print("You are too old 😂")
+  else: 
+    print("You are too young 🙂")
