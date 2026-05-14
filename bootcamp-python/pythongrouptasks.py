8b876adb3d65146e4459d15111573f00b573d162
Mordecai K. <39312305+insidemordecai@users.noreply.github.com>
2026-05-14 13:37:57 +0300

    update(bootcamp): conditional statements with python

---
+++ bootcamp-python/pythongrouptasks.py
@@ -0,0 +1,14 @@
+#conditional statements are used to check if aa certain condition is met 
+
+#real life example:home security system
+
+
+passcode = int(input("what is the passcode "))
+
+if passcode == "2435":
+  print("welcome home")
+else:
+  print ("get out")
+
+# this code is used to check if the passcode is correct
+# this technology is used by and safe home
